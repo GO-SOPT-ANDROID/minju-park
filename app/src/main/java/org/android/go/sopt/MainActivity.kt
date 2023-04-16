ee2e9ffbf2f6f7070a85bc8923f8c40af0d59672
minju1459 <76741702+minju1459@users.noreply.github.com>
2023-04-17 00:50:28 +0900

    Recyclerview 구현및 header 생성

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -35,27 +35,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnSignup.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startForResult.launch(intent)
 
-        }
-        binding.btnLogin.setOnClickListener {
-            //signupActivity의 id,specailty 값 = 입력 값 같을 때 로그인 성공 toast
 
-            if (binding.etId.text.toString() == id && binding.etPw.text.toString() == pw) {
-                Toast.makeText(this, getString(R.string.login_success), Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, IntroduceActivity::class.java).apply {
-                    //name,specailty 데이터 전달
-                    putExtra("name", name)
-                    putExtra("specialty",specialty)
-                }
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, getString(R.string.login_fail), Toast.LENGTH_SHORT).show()
-            }
 
-        }
+
     }
 }
 
