0ce73b8bc3e19aa39ced830d57633c63b1f98cf9
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-05-12 14:55:28 +0900

    [fix] SignInDTO 수정

--- app/src/main/java/org/android/go/sopt/data/remote/service/ReqresService.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/service/ReqresService.kt
@@ -4,6 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ReqresService {
-    @GET("/api/users?page=2")
+    @GET("api/users?page=2")
     fun getMembers(): Call<ResponseReqresDto>
 }
