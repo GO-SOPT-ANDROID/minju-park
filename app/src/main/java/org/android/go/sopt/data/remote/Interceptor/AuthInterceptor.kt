f35c586439546ffb82435e76571118fb950c63d7
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-06-01 21:20:53 +0900

    AuthInterceptor .header name 수정

--- app/src/main/java/org/android/go/sopt/data/remote/Interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/Interceptor/AuthInterceptor.kt
@@ -7,7 +7,7 @@ class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val headerRequest = originalRequest.newBuilder()
-            .header("Authorization <- 키 값은 이걸로 주로 사용합니다.", "")
+            .header("Authorization", "")
             .build()
 
         return chain.proceed(headerRequest)
