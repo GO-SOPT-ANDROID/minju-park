66cc295f0b38617fb0f4bdebfc446afeeb51f442
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-06-01 21:18:55 +0900

    Home UI Layer

--- app/src/main/java/org/android/go/sopt/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/HomeFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.android.go.sopt.data.remote.ServicePool
 import org.android.go.sopt.data.remote.model.ResponseReqresDto
 import org.android.go.sopt.databinding.FragmentHomeBinding
-import retrofit2.Call
-import retrofit2.Response
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -18,6 +17,7 @@ class HomeFragment : Fragment() {
         get() = requireNotNull(_binding) { " 앗! _binding이 null이다!" }
 
     private val memberService = ServicePool.reqresService
+    private val viewModel by viewModels<HomeViewmodel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,38 +31,22 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        memberService.getMembers().enqueue(object : retrofit2.Callback<ResponseReqresDto> {
-            override fun onResponse(
-                call: Call<ResponseReqresDto>,
-                response: Response<ResponseReqresDto>,
-            ) {
-                if (response.isSuccessful) {
-                    Snackbar.make(
-                        binding.root,
-                        "프로필 조회 성공",
-                        Snackbar.LENGTH_SHORT,
-                    ).show()
+        viewModel.getUser()
+        viewModel.getUserResult.observe(viewLifecycleOwner) { getUserResult ->
+            connetAdapter(getUserResult.data)
+        }
+    }
 
-                    binding.rvMember.adapter = RVAdapter()
-                        .apply {
-                            submitList(response.body()?.data)
-                        }
-                } else {
-                    Snackbar.make(
-                        binding.root,
-                        "프로필 조회 실패1",
-                        Snackbar.LENGTH_SHORT,
-                    ).show()
-                }
-            }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 
-            override fun onFailure(call: Call<ResponseReqresDto>, t: Throwable) {
-                Snackbar.make(
-                    binding.root,
-                    "서버 통신 실패",
-                    Snackbar.LENGTH_SHORT,
-                ).show()
-            }
-        })
+    private fun connetAdapter(userList: List<ResponseReqresDto.ReqresData>) {
+        val rvAdapter = RVAdapter()
+        rvAdapter.submitList(userList)
+
+        binding.rvMember.adapter = rvAdapter
+        binding.rvMember.layoutManager = LinearLayoutManager(requireContext())
     }
 }
