e629dc4a068828b95035f00cbd5db2a030fa1037
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-06-07 23:35:10 +0900

    [FIX] service Query 적용

--- app/src/main/java/org/android/go/sopt/data/remote/service/ReqresService.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/service/ReqresService.kt
@@ -2,8 +2,11 @@ package org.android.go.sopt.data.remote.service
 import org.android.go.sopt.data.remote.model.ResponseReqresDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ReqresService {
     @GET("api/users?page=2")
-    fun getMembers(): Call<ResponseReqresDto>
+    fun getMembers(
+        @Query("page") page: Int,
+    ): Call<ResponseReqresDto>
 }
